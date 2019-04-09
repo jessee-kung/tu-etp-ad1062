91e580365b86b35d54b904949497e8c1f563d15a
jessee_kung <jessee_kung@trendmicro.com>
2019-04-09 20:53:46 +0800

    [demo02] Update the demo for the linear classifier, implement Perceptron algorithm for visualization purposes

--- mlfund/dataset.py
+++ mlfund/dataset.py
@@ -13,13 +13,15 @@ class GaussianParam(object):
 
 class Gaussian(object):
     @staticmethod
-    def generate(params):
-        if not isinstance(params, list):
-            raise TypeError("'params' must be a list")
+    def generate(params, label_type='numeric'):
+        assert isinstance(params, list), '`params` must be a %s' % list.__name__
 
         K = len(params)
-        if K == 0:
-            raise TypeError("The number of categories (i.e., length of 'params') must be greater than 0")
+        assert K > 0, 'Length of `params` must be greater than 0'
+        assert label_type == 'numeric' or label_type == 'positive_negative', '`label_type` must be \'numeric\' or \'positive_negative\''
+
+        if label_type == 'positive_negative':
+            assert K == 2, 'Length of `params` must be 2 if `label_type` == \'positive_negative\''
 
         X = None
         y = None
@@ -38,12 +40,19 @@ class Gaussian(object):
 
             if not isinstance(X, numpy.ndarray):
                 X = numpy.random.multivariate_normal(param.mean, param.cov, param.N)
-                y = numpy.ones(param.N, int) * idxK
+
+                if label_type == 'positive_negative':
+                    y = numpy.ones(param.N, int) * -1
+                else:
+                    y = numpy.ones(param.N, int) * idxK
             else:
                 X2 = numpy.random.multivariate_normal(param.mean, param.cov, param.N)
                 X = numpy.append(X, X2, 0)
 
-                y2 = numpy.ones(param.N, int) * idxK
+                if label_type == 'positive_negative':
+                    y2 = numpy.ones(param.N, int) * 1
+                else:
+                    y2 = numpy.ones(param.N, int) * idxK
                 y = numpy.append(y, y2, 0)
 
         return (X, y)
